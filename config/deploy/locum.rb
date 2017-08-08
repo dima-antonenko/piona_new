0884132560ef222dd83fabf57ec6c5819041143a
dima_antonenko <d.antonenkod94@mail.ru>
2017-08-08 09:33:29 +0300

    update

---
+++ config/deploy/locum.rb
@@ -0,0 +1,6 @@
+role :app, %w(hosting_dima1212@cobalt.locum.ru)
+role :web, %w(hosting_dima1212@cobalt.locum.ru)
+role :db, %w(hosting_dima1212@cobalt.locum.ru)
+
+set :ssh_options, forward_agent: true
+set :rails_env, :production
